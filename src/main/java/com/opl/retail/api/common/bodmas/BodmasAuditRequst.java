96ed3fb60dffb0352ec47f764322d266ec343c30
yashwant.menaria <yashwant.menaria@CWDT092.corp.capitaworld.com>
2020-08-07 19:22:21 +0530

    add files

---
+++ src/main/java/com/opl/retail/api/common/bodmas/BodmasAuditRequst.java
@@ -0,0 +1,63 @@
+package com.opl.retail.api.common.bodmas;
+public class BodmasAuditRequst {
+
+	Long id;
+	private Long applicationId;
+	private String message;
+	private Date createdDate;
+	private Long createdBy;
+	private String data;
+	
+	public BodmasAuditRequst() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getApplicationId() {
+		return applicationId;
+	}
+
+	public void setApplicationId(Long applicationId) {
+		this.applicationId = applicationId;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Long getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(Long createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+}
---
+++ src/main/java/com/opl/retail/api/common/bodmas/BodmasResponse.java
@@ -0,0 +1,45 @@
+package com.opl.retail.api.common.bodmas;
+public class BodmasResponse {
+
+	
+	private String message;
+	private Integer status;
+	private Object data;
+	
+	
+	public BodmasResponse() {
+	}
+		
+	public BodmasResponse(String message, Integer status) {
+		this.message = message;
+		this.status = status;
+	}
+	
+	public BodmasResponse(String message, Integer status, Object data) {
+		super();
+		this.message = message;
+		this.status = status;
+		this.data = data;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	
+}
